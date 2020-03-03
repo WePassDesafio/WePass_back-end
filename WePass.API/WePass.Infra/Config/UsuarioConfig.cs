d7c231c56a080de3d3caae7edfcbd9d01f432ef7
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2020-03-03 19:00:19 -0300

    Começando a criar Migrations

---
+++ WePass.API/WePass.Infra/Config/UsuarioConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WePass.Infra.Entities;
+
+namespace WePass.Infra.Config
+{
+    public class UsuarioConfig : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario");
+            builder.HasKey(x => x.Id).HasName("Id_Usuario");
+            builder.Property(x => x.Id).HasColumnName("Id");
+        }
+    }
+}
